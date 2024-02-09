acbcb0398d67819211bd79edf89e72afa8e63c01
Christian Nagel <christian@christiannagel.com>
2024-02-09 14:03:27 +0100

    initial c# samples

---
+++ csharp/05b-Interceptors/MyInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Interceptors;
+public static class MyInterceptor
+{
+    [InterceptsLocation(@"C:\githublearn\bastamainz2023\csharp\05b-Interceptors\Runner.cs", 7, 9)]
+    public static void InterceptDotheMagic(int x)
+    {
+        Console.WriteLine($"Intercepting DotheMagic with {x}");
+    }
+
+    [InterceptsLocation(@"C:\githublearn\bastamainz2023\csharp\05b-Interceptors\Runner.cs", 9, 9)]
+    public static void FooMagic(this Runner r)
+    {
+        Console.WriteLine($"Intercepting Foo");
+    }
+}
