61babf79bc9a756b2e014d90003f02c75d5eb204
Christian Nagel <christian@christiannagel.com>
2024-02-12 22:30:45 +0100

    yarp sample

---
+++ yarp/YARPSample/TestMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace YARPSample;
+
+public class TestMiddleware(RequestDelegate next)
+{
+    public Task Invoke(HttpContext httpContext)
+    {
+        return next(httpContext);
+    }
+}
+
+// Extension method used to add the middleware to the HTTP request pipeline.
+public static class TestMiddlewareExtensions
+{
+    public static IApplicationBuilder UseTestMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<TestMiddleware>();
+    }
+}
