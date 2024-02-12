11de186fc393a4f231be69386fde33ce9ae079c0
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-02-12 19:13:57 +0100

    Added initial version of HotChocolate sample with HC13.8.1

---
+++ hotchocolate/BookSample.GraphQL/GraphQL/DataLoader/ReviewBatchDataloader.cs
@@ -0,0 +1,27 @@
+﻿using BookSample.ReviewAPIClient;
+using BookSample.ReviewAPIClient.Models;
+
+namespace BookSample.GraphQL.GraphQL.DataLoader;
+
+public class ReviewBatchDataloader(ReviewsClient reviewsClient, IBatchScheduler batchScheduler, DataLoaderOptions? options = null) : BatchDataLoader<long, IEnumerable<Review>>(batchScheduler, options)
+{
+    private static readonly List<Review> s_emptyReviewList = [];
+
+    internal int? TakeParameter { get; set; }
+
+    protected override async Task<IReadOnlyDictionary<long, IEnumerable<Review>>> LoadBatchAsync(IReadOnlyList<long> keys, CancellationToken cancellationToken)
+    {
+        var result = await reviewsClient.Api
+            .Reviews
+            .GetAsync(config =>
+            {
+                config.QueryParameters.BookIds = keys.ToStrings();
+                config.QueryParameters.Take = TakeParameter;
+            }, cancellationToken)
+            ?? s_emptyReviewList;
+        return result
+            .Where(x => x.BookId != default)
+            .GroupBy(x => x.BookId!.Value)
+            .ToDictionary(x => x.Key, x => x.AsEnumerable());
+    }
+}
---
+++ hotchocolate/BookSample.GraphQL/GraphQL/Mutations/Inputs/UpdateBookInput.cs
@@ -0,0 +1,7 @@
+﻿namespace BookSample.GraphQL.GraphQL.Mutations.Inputs;
+
+public class UpdateBookInput : SaveBookInput
+{
+    [ID]
+    public required long BookId { get; set; }
+}
