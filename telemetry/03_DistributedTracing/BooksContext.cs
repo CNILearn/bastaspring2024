22fad67312b96aa1cb1e4d645b8e3bcf6fc33619
Christian Nagel <christian@christiannagel.com>
2024-02-12 21:46:39 +0100

    logging, metrics, distributed tracing

---
+++ telemetry/03_DistributedTracing/BooksContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiagnosticsSample;
+
+public class BooksContext(DbContextOptions<BooksContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books => Set<Book>();
+}
+
+public record Book(string Title, string Publisher, int BookId = default);
