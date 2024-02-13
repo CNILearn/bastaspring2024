1c92d977b663f49eaa3c99b29b11088273673b0a
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-02-13 13:54:42 +0100

    Added Uno Island (WPF) sample

---
+++ xaml/uno/UnoIslandsSampleApp/UnoIslandsSampleApp.Shared/UnoPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace UnoIslandsSampleApp;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class UnoPage : Page
+{
+    public UnoPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ xaml/uno/UnoIslandsSampleApp/WpfApp/WpfContent.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp;
+/// <summary>
+/// Interaction logic for WpfContent.xaml
+/// </summary>
+public partial class WpfContent : UserControl
+{
+    public WpfContent()
+    {
+        InitializeComponent();
+    }
+}
